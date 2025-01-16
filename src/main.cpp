caac4581db7d14795674f1138fb8aec601d1a936
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2025-01-16 13:28:31 +0100

    definition of Inode

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,20 @@
 const int BLOCK_SIZE = 512;
 const int NUM_BLOCKS = 48;
 const int NUM_INODES = 16;
+const int NUM_FILES = 16;
 
 struct Superblock {
-    int magic;
-    int num_blocks;
-    int free_blocks ;
+    int magic = 0xf0f03410;
+    int num_blocks = NUM_BLOCKS;
+    int free_blocks = NUM_BLOCKS;
     int inode_start = 1;
+};
+
+struct Inode {
+    char name[50];
+    int size;
+    int block_pointers[NUM_FILES];
+    int used = false;
 };
 
 
